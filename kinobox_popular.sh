8a3e15d21154558c295c176da70d3ed7210c6ed2
Andrey <kms.amur@gmail.com>
2025-02-01 00:52:05 +1000

    update: kinobox, ru* year

--- kinobox_popular.sh
+++ kinobox_popular.sh
@@ -5,16 +5,17 @@ source ./text-color.sh
 
 host=$(curl -s --location 'https://kinobox.tv/go/demo' | \
        grep "location.href = '" | \
-       sed "s/location.href = '//g ; s/...$//g" | awk '{print $1}')
+       sed "s/location.href = '//g ; s/...$//g" | \
+       awk '{print $1}' | sed 's/\//\\\//g')
+host="${host}\/film\/"
 
-for type in film series; do 
-  echo -e "${red}Популярные ${type}"
-  title_film=$(curl -s "https://kp.kinobox.tv/films/popular?${type}=true")
-  for number in {0..49}; do 
-    title=$(echo -n "${title_film}" | jq -r ".data.films.[$number].title.russian")
-    id=$(echo -n "${title_film}" | jq -r ".data.films.[$number].id")
-    echo -e "${blue}${title}: ${violet}${host}/film/${id}"
-    done
-  done
-echo -ne "${normal}"
+popular=$(curl -s "https://kp.kinobox.tv/films/popular")
+
+for type in Film Series; do
+  echo -e "${red}Популярные ${type}${normal}"
+  title=$(echo -n "${popular}" | \
+          jq -r ".data.films.[] | select(.type == \"${type}\") | .title.russian, .id" | \
+          sed "N;s/\n/ 👉 ${host}/")
+  echo -e "${blue}${title}${normal}"
+done
 
